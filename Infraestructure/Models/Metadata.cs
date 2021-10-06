da2efab2d0ad8cfbfcb2be5d7100825de83cea0a
Omar Picado <ariielpb@gmail.com>
2021-10-06 17:14:41 -0600

    Se crea el modelo y se agregan las clases Metadata y MyContext

---
+++ Infraestructure/Models/Metadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Models
+{
+    //Se crea un internal partial class para cada clase con su nombre "nombreClaseMetadata",
+    //y allí se hacen los DataAnotations de cada clase
+    internal partial class ActivoMetadata
+    {
+
+    }
+}
